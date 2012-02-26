52dd0b51b7b8d26d06d85374bcf9f0ff45d7b7d3
Casey Stehlik <StehlikC@gmail.com>
2012-02-25 23:54:36 -0600

    Stub out implementations for the foot and inch units

--- lib/measurements/unit.rb
+++ lib/measurements/unit.rb
@@ -12,6 +12,8 @@ module Measurements
         ABBREVIATIONS = YAML::load(File.open(File.join(File.dirname(__FILE__), 'config', 'abbreviations.yml')))
         
         autoload :BaseUnit,   'measurements/unit/baseunit'
+        
+        # Cooking Units
         autoload :Ounce,      'measurements/unit/ounce'
         autoload :Pound,      'measurements/unit/pound'
         autoload :Teaspoon,   'measurements/unit/teaspoon'
@@ -20,6 +22,10 @@ module Measurements
         autoload :Pint,       'measurements/unit/pint'
         autoload :Quart,      'measurements/unit/quart'
         autoload :Gallon,     'measurements/unit/gallon'
+        
+        # Imperial Units
+        autoload :Inch,       'measurements/unit/inch'
+        autoload :Foot,       'measurements/unit/foot'
                 
         # Helper method to get what units are currently implemented
         # @deprecated Use {Measurements#available_units} instead of this method because
