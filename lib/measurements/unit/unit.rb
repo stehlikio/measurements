0ba12014f978947ebac8b57a6c21d8070f12d48f
Casey Stehlik <StehlikC@gmail.com>
2012-03-04 02:20:54 -0600

    Add Documentation
    Documented all of the new functions in Unit and also updated the README
    a bit

--- lib/measurements/unit/unit.rb
+++ lib/measurements/unit/unit.rb
@@ -9,46 +9,61 @@ module Measurements
                 @klass = klass
             end
 
+            # @see Measurements::Unit::BaseUnit#inspect
             def inspect
                 self.quantity
             end
 
+            # A pretty print method that will return a human readable version
+            #   of a unit
+            # @return [String] a human readable unit description
             def humanize
-                self.quantity.to_s + self.unit_abbr
+                return "#{self.quantity.to_s} #{self.unit_abbr}"
             end
 
+            # @see Measurements::Unit::BaseUnit#type=
             def type=(type)
                 @klass.type = type
             end
 
+            # @see Measurements::Unit::BaseUnit#type
             def type
                 @klass.type
             end
 
+            # @see Measurements::Unit::BaseUnit#inspect
             def quantity
                 @klass.quantity
             end
 
+            # @see Measurements::Unit::BaseUnit#inspect
             def quantity=(quantity)
                 @klass.quantity = quantity
             end
 
+            # @see Measurements::Unit::BaseUnit#unit
             def unit
                 @klass.unit
             end
 
+            # @see Measurements::Unit::BaseUnit#unit_type
             def unit_type
                 @klass.unit_type
             end
 
+            # @see Measurements::Unit::BaseUnit#unit_system
             def unit_system
                 @klass.unit_system
             end
 
+            # @see Measurements::Unit::BaseUnit#unit_abbr
             def unit_abbr
                 @klass.unit_abbr
             end
 
+            # This method will call the current units convert_to method. This is the dangerous version and it will
+            #   change the callers unit.
+            # @see Measurements::Unit::BaseUnit#convert_to
             def convert_to!(type)
                 @klass = @klass.convert_to(type)
             end
